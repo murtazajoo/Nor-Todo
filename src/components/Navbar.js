c28c655a965743d5677514a414544cfc8e1cb92a
murtazajoo <jmtariq2003@gmail.com>
2023-08-31 19:28:31 +0530

    menu Bug fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -26,8 +26,10 @@ function Navbar() {
   useEffect(() => {
     if (menuOpen) {
       document.body.style.overflow = "hidden";
+      document.body.style.height = "100vh";
     } else {
       document.body.style.overflow = "unset";
+      document.body.style.height = "unset";
     }
   }, [menuOpen]);
 
